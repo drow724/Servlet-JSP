12e5e7242b08e478340909485af1334bf469f8aa
Macintosh <>
2021-02-03 14:32:49 +0900

    Servlet Filter

--- JSPPrj/src/com/newlecture/web/NoticeReg.java
+++ JSPPrj/src/com/newlecture/web/NoticeReg.java
@@ -15,11 +15,9 @@ public class NoticeReg extends HttpServlet {
 			
 			response.setCharacterEncoding("UTF-8");
 			response.setContentType("text/html; charset=UTF-8");
-			request.setCharacterEncoding("UTF-8");
+			//request.setCharacterEncoding("UTF-8");
 			
 			PrintWriter out = response.getWriter();
-			
-			
 			
 			String title = request.getParameter("title");
 			String content = request.getParameter("content");
