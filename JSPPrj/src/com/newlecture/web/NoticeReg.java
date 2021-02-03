104d76f69de67e00282a054447c31067a87cbaf6
Macintosh <>
2021-02-03 12:25:00 +0900

    한글 입력 문제

--- JSPPrj/src/com/newlecture/web/NoticeReg.java
+++ JSPPrj/src/com/newlecture/web/NoticeReg.java
@@ -15,6 +15,7 @@ public class NoticeReg extends HttpServlet {
 			
 			response.setCharacterEncoding("UTF-8");
 			response.setContentType("text/html; charset=UTF-8");
+			request.setCharacterEncoding("UTF-8");
 			
 			PrintWriter out = response.getWriter();
 			
