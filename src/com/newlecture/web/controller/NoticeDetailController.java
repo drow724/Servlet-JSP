dde54be89c4cb9c0ce25935113f49428366f2e08
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-03-22 17:14:54 +0900

    자세한 페이지 수정하기

--- src/com/newlecture/web/controller/NoticeDetailController.java
+++ src/com/newlecture/web/controller/NoticeDetailController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
 
 @WebServlet("/notice/detail")
 public class NoticeDetailController extends HttpServlet {
@@ -19,49 +20,11 @@ public class NoticeDetailController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		   int id = Integer.parseInt(request.getParameter("id"));
+	
+		   NoticeService service = new NoticeService();
+		   Notice notice = service.getNotice(id);
+		   request.setAttribute("n", notice);
 		   
-		   	String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-			String sql = "SELECT * FROM NOTICE WHERE ID=?";
-			
-			try {
-				Class.forName("oracle.jdbc.driver.OracleDriver");
-				Connection con = DriverManager.getConnection(url, "NEWLEC", "119562");
-				PreparedStatement st = con.prepareStatement(sql);
-				st.setInt(1, id);
-				
-				ResultSet rs = st.executeQuery();
-			   
-				rs.next();
-				
-			 	String title = rs.getString("TITLE");
-			 	Date regDate = rs.getDate("REGDATE");
-			 	String content = rs.getString("CONTENT");
-			 	String files = rs.getString("FILES");
-			 	String writerId = rs.getString("WRITER_ID");
-			 	String hit = rs.getString("HIT");
-			    
-			 	Notice notice = new Notice(
-			 			id,
-			 			title,
-			 			regDate,
-			 			content,
-			 			files,
-			 			writerId,
-			 			hit
-			 			);
-			 	
-			 	request.setAttribute("n", notice);
-			 	
-			    rs.close();
-					st.close();
-					con.close();
-			} catch (ClassNotFoundException | SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-
-			
 			request.getRequestDispatcher("/WEB-INF/view/notice/detail.jsp").forward(request, response);
 	}
 }
